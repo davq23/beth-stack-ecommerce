876c75fd9860cd5518ce68eac59efbe9449a301b
davq23 <davidquinterogranadillo@gmail.com>
2023-09-23 18:07:13 -0400

    Add no result

--- src/components/generic/search-result.tsx
+++ src/components/generic/search-result.tsx
@@ -5,7 +5,10 @@ interface SearchResultsProps {
 }
 
 export const SearchResults = ({results}: SearchResultsProps) => {
-    return <div class="dropdown-menu show" role="menu">
-        {results.map(({name, href}) => <a class="dropdown-item" href={href}>{name}</a>)}
+    return <div class="dropdown-menu show" role="menu"> {
+        results.length > 0 ? results.map(
+            ({name, href}) => <a class="dropdown-item" href={href}>{name}</a>
+        ) : <div class="dropdown-item">No results found</div>
+    }
     </div>
 };
