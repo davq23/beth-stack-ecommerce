6e47c0b0faa10c8225d33b330fa1776b84e9b710
davq23 <davidquinterogranadillo@gmail.com>
2023-09-23 15:12:38 -0400

    Add home card

--- src/router/home.tsx
+++ src/router/home.tsx
@@ -9,6 +9,16 @@ const homeRoutes = new Elysia().group(
     app => app.use(html()).get(
         '',
         () => <BaseTemplate>
+            <div class="container">
+                <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title" safe>BETH Stack E-commerce</h4>
+                    <p class="card-text" safe>
+                        Page created using the BETH stack
+                    </p>
+                </div>
+                </div>
+            </div>
         </BaseTemplate>
     )
 );
