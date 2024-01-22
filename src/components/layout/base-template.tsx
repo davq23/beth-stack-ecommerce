add6e8c5b32fa3b95ac1990bbeab9731081185a3
davq23 <davidquinterogranadillo@gmail.com>
2024-01-21 23:30:36 -0400

    Add meta description

--- src/components/layout/base-template.tsx
+++ src/components/layout/base-template.tsx
@@ -13,6 +13,7 @@ export const BaseTemplate = ({ children }: elements.Children) => (
             <head>
                 <meta charset="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="description" content="E-commerce page created with the BETH stack" />
                 <title>Beth Stack</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css"></link>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
