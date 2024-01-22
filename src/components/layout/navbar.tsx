3d01e3e61868e892a88fb0894835cc26929bff37
davq23 <davidquinterogranadillo@gmail.com>
2024-01-21 22:29:33 -0400

    Add floating search label + svg cart icon

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -14,13 +14,14 @@ export const Navbar = () => {
         <div class="d-flex justify-content-center my-1 align-items-center">
             <SearchBar className="position-relative mb-0" swap='outerHTML' target='find .dropdown-menu' endpoint={addBaseUrl('products/search')} placeholder='Search for a product' inputDelayMs={300}>
                 <div
-                    class="dropdown"
+                    class="dropdown form-floating"
                     data-hx-boost="true"
                     data-hx-target="#main"
                     data-hx-swap="innerHTML"
                     _="on click remove .show from .dropdown-menu in me send resetSearch to closest <form />"
                 >
-                    <input type="text" class="form-control dropdown-toggle" name="q" id="query" placeholder={'Search for a product'} data-bs-toggle="dropdown" aria-expanded="false"/>
+                    <input type="text" class="form-control dropdown-toggle" name="q" id="query" placeholder={'...'} data-bs-toggle="dropdown" aria-expanded="false"/>
+                    <label for="query">Search for a product</label>
                     <div class="dropdown-menu"></div>
                 </div>
             </SearchBar>
@@ -38,9 +39,11 @@ export const Navbar = () => {
 
         on click toggle @disabled until htmx:afterOnLoad"
         >
-            <i class="bi bi-cart"></i>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+            </svg>
             <span class="currency"></span>
-            <span class="total"></span>
+            <span class="total">My Cart</span>
         </button>
     </nav>
 };
