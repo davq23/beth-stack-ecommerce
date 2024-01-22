fe42aad49e4b6554c04865544980d0bfbbfcd1ca
davq23 <davidquinterogranadillo@gmail.com>
2024-01-21 23:30:46 -0400

    Add search bar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -20,9 +20,14 @@ export const Navbar = () => {
                     data-hx-swap="innerHTML"
                     _="on click remove .show from .dropdown-menu in me send resetSearch to closest <form />"
                 >
-                    <input type="text" class="form-control dropdown-toggle" name="q" id="query" placeholder={'...'} data-bs-toggle="dropdown" aria-expanded="false"/>
-                    <label for="query">Search for a product</label>
-                    <div class="dropdown-menu"></div>
+                    <input type="text" class="form-control dropdown-toggle" name="q" id="query" placeholder={'...'} data-bs-toggle="dropdown" />
+                    <label for="query">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                        </svg>
+                        Search for a product
+                    </label>
+                    <div class="dropdown-menu" aria-expanded="false"></div>
                 </div>
             </SearchBar>
         </div>
